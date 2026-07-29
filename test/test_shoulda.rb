22d5ce62f930c487be9e41179c5f62e29675b241
cub8 <tsubait8@gmail.com>
2026-07-29 21:55:01 +0200

    Remove default test

--- test/test_shoulda.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class Ruby::Lsp::TestShoulda < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::RubyLsp::Shoulda::VERSION
-  end
-end
